905e6d8d53e2b12ee92b04e89e4d1b2a2a1f8f14
altrono <hbotema16@gmail.com>
2025-01-10 16:28:30 +0200

    Top header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/header";
+import { WhatsAppButton } from "./components/whatsap-button";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Header />
+      <WhatsAppButton />
     </main>
   );
 }
