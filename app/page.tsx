26cbee44c845b4b5996681f05d9539c79b226bfd
altrono <hbotema16@gmail.com>
2025-01-10 15:34:01 +0200

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <p>Welcome</p>
       <h1>Mirasal (PTY) LTD Boss Erick Limbaya</h1>
     </div>
