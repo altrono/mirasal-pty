2d8eb6fd5e65a438793bd4d473e145b6ca243005
altrono <hbotema16@gmail.com>
2025-01-11 16:53:32 +0200

    Add loading spinner

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function LoadingSpinner() {
+    return (
+      <div className="flex justify-center items-center h-20">
+        <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    )
+  }
+  
+  
